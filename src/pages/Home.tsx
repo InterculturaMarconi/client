f8d23340f832dd81132192f515f5fb0ba27ec7a6
AgraMantis <nicolas.fiocco.lt@gmail.com>
2022-05-06 22:42:26 +0200

    aggiunta al config percorso @/img e img in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '@components/Navbar';
 import { Card } from '@mui/material';
+import TrePersoneMondo from '@/img/TrePersoneMondo.png';
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <img src={TrePersoneMondo} height={400} />
         </>
     )
 }
