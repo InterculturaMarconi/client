a6fccde3a8f1ea56370fc851b8720e20d32bfcc1
danieleguglietti <gugliettidaniele@gmail.com>
2022-05-06 13:57:21 +0200

    aggiunto redux

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
     reducer: {}
@@ -6,3 +7,6 @@ export const store = configureStore({
 
 export type Dispatcher = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+
+export const useAppDispatch = () => useDispatch<Dispatcher>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
