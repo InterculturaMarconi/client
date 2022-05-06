f3b38546b591e6226c22bfa46641365770f41e69
AgraMantis <nicolas.fiocco.lt@gmail.com>
2022-05-06 14:19:45 +0200

    aggiunta navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Navbar from '../components/Navbar';
 const Home = () => {
     return (
         <>
+            <Navbar />
             <h1>Hello World!</h1>
             <p>Test Paragraph.</p>
         </>
