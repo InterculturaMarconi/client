52ce06c1a0f0ff8c42ca8d7ce466de3767499c11
AgraMantis <nicolas.fiocco.lt@gmail.com>
2022-05-07 10:41:08 +0200

    testo paginato

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,30 @@ import Navbar from '~/components/Navbar';
 import Table from '~/components/Table';
 import Page from '~/components/Page';
 import TrePersoneMondo from '~/img/TrePersoneMondo.png';
+import { Box } from '@mui/system';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
 
 const Home: React.FC = () => (
     <Page title="Home | InterculturaMarconi">
         <Navbar />
-        <img src={TrePersoneMondo} height={400} alt="" />
+        <Grid container spacing={1} alignItems="center" justify="left">
+            <img src={TrePersoneMondo} height={400} alt="" />
+            <Box width={600}>
+                <Typography variant="h3" gutterBottom component="div">
+                    Agenda 2030
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    L’Agenda 2030 per lo Sviluppo Sostenibile è un programma d’azione per le persone, il pianeta e la
+                    prosperità sottoscritto nel settembre 2015 dai governi dei 193 Paesi membri dell’ONU. Essa ingloba
+                    17 Obiettivi per lo Sviluppo Sostenibile – Sustainable Development Goals, SDGs – in un grande
+                    programma d’azione per un totale di 169 ‘target’ o traguardi. L’avvio ufficiale degli Obiettivi per
+                    lo Sviluppo Sostenibile ha coinciso con l’inizio del 2016, guidando il mondo sulla strada da
+                    percorrere nell’arco dei prossimi 15 anni: i Paesi, infatti, si sono impegnati a raggiungerli entro
+                    il 2030.
+                </Typography>
+            </Box>
+        </Grid>
         <Table />
     </Page>
 );
