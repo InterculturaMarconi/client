37c1568772b07762f9306fa275024ef7bc0fe652
AgraMantis <nicolas.fiocco.lt@gmail.com>
2022-05-07 09:55:32 +0200

    component table

---
+++ src/components/Table.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+const Table = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={1}>
+                <Grid container item spacing={3}>
+                    {/* <FormRow /> */}
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default Table;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Navbar from '~/components/Navbar';
+import Table from '~/components/Table';
 import Page from '~/components/Page';
 import TrePersoneMondo from '~/img/TrePersoneMondo.png';
 
@@ -8,6 +9,7 @@ const Home: React.FC = () => (
     <Page title="Home | InterculturaMarconi">
         <Navbar />
         <img src={TrePersoneMondo} height={400} alt="" />
+        <Table />
     </Page>
 );
 
