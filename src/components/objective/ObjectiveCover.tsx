dc017eaedcde068e5a8248ffda78150059f16d30
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-05-10 17:07:52 +0200

    cover obiettivo

---
+++ src/components/objective/ObjectiveCover.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ObjectiveCover: React.FC = () => {
+    return <h1>ObjectiveCover Page</h1>;
+};
+
+export default ObjectiveCover;
--- src/pages/obiettivo/Obiettivo14.tsx
+++ src/pages/obiettivo/Obiettivo14.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import Navbar from '~/components/Navbar';
-import ObjectivesTable from '~/components/objective/ObjectivesTable';
 import Page from '~/components/Page';
-import Footer from '~/components/footer/Footer';
 import fishBackground from '~/img/obb14/img1obb14.jpg';
-import { Box } from '@mui/system';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
 
 const Obiettivo14: React.FC = () => (
     <Page title="Obiettivo 14 | InterculturaMarconi">
-        <Navbar />
         <Typography variant="h3" gutterBottom component="div">
             Obiettivo 14
         </Typography>
@@ -19,7 +13,6 @@ const Obiettivo14: React.FC = () => (
             La vita sott'acqua
         </Typography>
         <img src={fishBackground} alt="" />
-        <Footer />
     </Page>
 );
 export default Obiettivo14;
